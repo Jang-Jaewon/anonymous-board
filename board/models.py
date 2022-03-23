aaf886778cc63f6161fac3c3027c1625880295dd
Jang-Jaewon <jewon119@gmail.com>
2022-03-23 15:06:42 +0900

    Add: user, board 앱 생성 및 Model 구축

---
+++ board/models.py
@@ -0,0 +1,23 @@
+from pyexpat import model
+from django.db import models
+
+from user.models import User
+
+
+class Post(models.Model):
+    user       = models.ForeignKey(User, on_delete=models.CASCADE)
+    title      = models.CharField(max_length=100)
+    content    = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    img_url    = models.URLField(null=True)
+    
+    class Meta:
+        db_table = 'post'
+        
+class Comment(models.Model):
+    post       = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content    = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        db_table = 'comment'
--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
+    'board',
 ]
 
 MIDDLEWARE = [
@@ -105,3 +107,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.User'
