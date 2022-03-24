9619a572bb537505bf78fddd40d780ba1bccb785
Jang-Jaewon <jewon119@gmail.com>
2022-03-24 16:07:30 +0900

    Add: 글쓰기 구현

--- board/urls.py
+++ board/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from board.views import board
+from board.views import board, post_write
 
 urlpatterns = [
     path('', board, name='board'),
+    path('post/write', post_write, name='post_write'),
 ]
--- board/views.py
+++ board/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from datetime import datetime
+import uuid
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
+from django.core.files.storage import default_storage
 
 from board.models import Post
 
@@ -18,3 +23,29 @@ def board(request):
             'search_text': search_text,
         }
         return render(request, 'page/index.html', context=context)
+
+
+@login_required(login_url='signin')
+def post_write(request):
+    if request.method == "GET":
+        return render(request, 'page/post_write.html')
+
+    if request.method == "POST":
+        title = request.POST['title']
+        content = request.POST['content']
+        img = request.FILES.get('img', None)
+        img_url = None
+        
+        if img:
+            img_name = uuid.uuid4()
+            ext = img.name.split('.')[-1]
+            default_storage.save(f"{img_name}.{ext}", img)
+            img_url = f"{img_name}.{ext}"
+
+        Post(
+            user=request.user,
+            title=title,
+            content=content,
+            img_url=img_url
+        ).save()
+        return redirect('board')
