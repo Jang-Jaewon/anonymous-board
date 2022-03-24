824db08a5203c036e6c8575631f4d48814ec68af
Jang-Jaewon <jewon119@gmail.com>
2022-03-24 15:15:45 +0900

    Add: 검색 기능 추가

--- board/views.py
+++ board/views.py
@@ -6,11 +6,15 @@ from board.models import Post
 
 def board(request):
     if request.method == 'GET':
-        page = request.GET.get('page', 1)
-        post_set = Post.objects.all().order_by('-id')
-        paginator = Paginator(post_set, 4)
-        post_set = paginator.get_page(page)
+        page        = request.GET.get('page', 1)
+        search_text = request.GET.get('search_text', "")
+        post_set    = Post.objects.filter(title__icontains=search_text).order_by('-id')
+        
+        paginator = Paginator(post_set, 6)
+        post_set  = paginator.get_page(page)
+        
         context = {
             'post_set': post_set,
+            'search_text': search_text,
         }
         return render(request, 'page/index.html', context=context)
