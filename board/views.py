a5cb16722f6009010ea80e5c2e779b9f4441ca65
Jang-Jaewon <jewon119@gmail.com>
2022-03-24 14:32:18 +0900

    Add: 게시판 리스트 추가

--- board/views.py
+++ board/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+from board.models import Post
+
 
 def board(request):
     if request.method == 'GET':
+        page = request.GET.get('page', 1)
+        post_set = Post.objects.all().order_by('-id')
+        paginator = Paginator(post_set, 4)
+        post_set = paginator.get_page(page)
         context = {
-            'title': 'Hello',
+            'post_set': post_set,
         }
         return render(request, 'page/index.html', context=context)
