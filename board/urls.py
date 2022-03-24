b53c97ac23de2ce41c2bc2303468f4e4eb23d53d
Jang-Jaewon <jewon119@gmail.com>
2022-03-24 17:16:17 +0900

    Add: 상세페이지 구현

--- board/urls.py
+++ board/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from board.views import board, post_write
+from board.views import board, post_write, post_detail
 
 urlpatterns = [
     path('', board, name='board'),
     path('post/write', post_write, name='post_write'),
+    path('post/<int:post_id>', post_detail, name="post_detail"),
 ]
